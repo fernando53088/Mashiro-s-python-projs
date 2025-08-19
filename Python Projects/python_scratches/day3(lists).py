f25641ea1870a2388136dae602bbe3f1e9952207
fernando53088 <144405711+fernando53088@users.noreply.github.com>
2025-08-20 07:18:03 +0800

    Add files via upload

---
+++ Python Projects/python_scratches/day3(lists).py
@@ -0,0 +1,10 @@
+import random
+foods = ['chicken', 'kfc chicken', 'wings']
+print(foods)
+numbers = []
+numbers.append(1)
+numbers.append(69)
+numbers.append(74)
+print(numbers)
+animals = ['chicken', 'cow', 'goat', 'pig', 'horse']
+print(random.choice(animals))
