8ad02f0ddd10d23323f2414de1498d609e2ada89
fernando53088 <144405711+fernando53088@users.noreply.github.com>
2025-08-20 07:19:00 +0800

    Delete day3_wordscrambler.py

--- day3_wordscrambler.py
+++
@@ -1,6 +0,0 @@
-import random
-scrambbler = input("Give me word\n") # input
-letters = list(scrambbler)
-random.shuffle(letters)
-final_shuffle = "".join(letters) # this is the rope that strings it all together
-print(final_shuffle)
